751250d4186dd6429326ac04ea22113b87fc9934
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-09-09 00:13:07 -0400

    Update JavaScript for different OCDS Show URL, and fix code style

--- docs/_static/show-shower.js
+++ docs/_static/show-shower.js
@@ -1,16 +1,16 @@
-$(function() {
-
-$("a[href*='_static/ocds-show']").click(function() { 
-$.modal('<iframe src="' + $(this).attr("href") + '" height="800" width="100%" style="border:0">', {
-    closeHTML:"",
-    containerCss:{
-        backgroundColor:"#fff", 
-        borderColor:"#fff", 
-        height:800, 
-        padding:0, 
-        width:"90%"
-    },
-    overlayClose:true
-}); ;return false });
-
-})
+$(function () {
+  $('a[href^="https://open-contracting.github.io/ocds-show-ppp/?load="]').click(function () {
+    $.modal('<iframe src="' + $(this).attr('href') + '" height="800" width="100%" style="border: 0">', {
+      closeHTML: '',
+      containerCss: {
+          backgroundColor: '#fff',
+          borderColor: '#fff',
+          height: 800,
+          padding: 0,
+          width: '90%'
+      },
+      overlayClose: true
+    });
+    return false;
+  });
+});
