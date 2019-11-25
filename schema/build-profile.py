62580f5222358920cd4f3679d8825fe76cf0c11f
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-25 13:50:47 -0500

    Run isort

--- schema/build-profile.py
+++ schema/build-profile.py
@@ -6,7 +6,7 @@ from ocdsextensionregistry import build_profile
 basedir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(os.path.join(basedir, '..', 'docs'))
 
-from conf import standard_tag, standard_version, extension_versions, schema_base_url, update_codelist_urls  # noqa
+from conf import standard_tag, standard_version, extension_versions, schema_base_url, update_codelist_urls  # noqa isort:skip
 
 build_profile(basedir, standard_tag, extension_versions, schema_base_url=schema_base_url,
               update_codelist_urls=update_codelist_urls)
