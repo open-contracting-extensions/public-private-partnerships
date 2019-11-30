0dee0f88942ecee1905688f4fef12b8d329b9300
James McKinney <26463+jpmckinney@users.noreply.github.com>
2019-11-29 22:04:33 -0500

    Update files from template

--- tests/__init__.py
+++ tests/__init__.py
@@ -8,7 +8,14 @@ test_basic_params = {
     'es': 'Inicio',
 }
 
+test_navigation_params = [
+    ('en', 'Next'),
+    ('es', 'Siguiente'),
+]
+
 test_search_params = [
     ('en', r'found \d+ page\(s\) matching'),
     ('es', r'encontró \d+ página\(s\) acorde'),
 ]
+
+last_path = '/governance/'
