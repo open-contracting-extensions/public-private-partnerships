c6b606e3f27905b6e4d6dfa06223944a9dda2352
James McKinney <26463+jpmckinney@users.noreply.github.com>
2020-01-21 20:13:09 -0500

    Update to latest profile template

--- include/config.mk
+++ include/config.mk
@@ -22,10 +22,18 @@ DIST_FILES=schema/profile/release-schema.json schema/profile/codelists schema/pa
 POT_DIR=$(BUILD_DIR)/locale
 # The prefix, if any, to the schema and codelists domains.
 DOMAIN_PREFIX=ppp-
-# Directory containing assets to copy to the build directory (no trailing slash).
-ASSETS_DIR=
 # The Transifex project name.
 TRANSIFEX_PROJECT=ocds-for-ppps
+# Any additional extract targets.
+EXTRACT_TARGETS=
+
+# The path to the branch of the documentation to print to PDF.
+PDF_ROOT=/profiles/ppp/latest
+# The pattern of pages to print to PDF. Update if the documentation adds, removes or renames pages.
+PDF_PAGES={,example/,framework/,governance/,overview/,spreadsheet/,technical/,timing/,extensions/,reference/{,browser/,changelog/,codelists/,documents/,schema/}}
+# 15000 may warn: "Warning: Received createRequest signal on a disposed ResourceObject's NetworkAccessManager. This might
+# be an indication of an iframe taking too long to load."
+PDF_DELAY=20000
 
 # Compile PO files for codelists and schema to MO files, so that `translate` succeeds.
 .PHONY: compile
