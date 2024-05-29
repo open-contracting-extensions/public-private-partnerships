89d0daa36c7c1bcf0897c452725ddee9b5ed3fef
James McKinney <26463+jpmckinney@users.noreply.github.com>
2024-05-28 21:42:25 -0400

    ci: Switch to Biome, preserving Standard style

--- docs/_static/show-shower.js
+++ docs/_static/show-shower.js
@@ -1,7 +1,7 @@
 /* global $ */
 
 $('a[href^="https://open-contracting.github.io/ocds-show-ppp/?load="]').click(function () {
-  $.modal('<iframe src="' + $(this).attr('href') + '" height="800" width="100%" style="border: 0">', {
+  $.modal(`<iframe src="${$(this).attr('href')}" height="800" width="100%" style="border: 0">`, {
     closeHTML: '',
     containerCss: {
       backgroundColor: '#fff',
